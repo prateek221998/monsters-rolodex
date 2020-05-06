0551be178dd16c4346fc5e679924933a90ee4bfe
unknown <dwivedi.prateek22@gmail.com>
2020-05-07 01:58:15 +0530

    Updates

---
+++ precache-manifest.910d7c3b3324c2264b28008293685844.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13fd4393c1048901467fde19ee26a452",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d6f7327639246e75c1cb",
+    "url": "/monsters-rolodex/static/css/main.54ad4668.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f7327639246e75c1cb",
+    "url": "/monsters-rolodex/static/js/main.5999a354.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
